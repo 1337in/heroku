ef60ed4d3e4e0ced4520b82fad709791d57e031d
x00 <32819740+x00x00@users.noreply.github.com>
2020-05-04 07:15:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <html>
 <center>
 <h1>
-Wow! Taken Over
+Wow! Website Taken Over!!!!
 by x00
-</html>
-</center>
 </h1>
+  <br>
+  <h3>Your own website can be use to do many bad things</h3>
+  <br>
+   <img src="https://media0.giphy.com/media/of41tbgMBUhQk/source.gif"> 
+  <br>
+  <h2>Please Fix!! xD</h2>
+</center>
+</html>
+
 
